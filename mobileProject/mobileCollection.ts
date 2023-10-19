ef8360c1a543247e0cea981227ac0532ef4b538f
Miguel Franco <francamente@hotmail.es>
2023-10-19 23:37:13 +0200

    + metodo privado totalPriceCalculation

--- mobileProject/mobileCollection.ts
+++ mobileProject/mobileCollection.ts
@@ -4,6 +4,7 @@ export class MobileCollection{
     private totalPrice: number
     constructor(private mobiles: Mobile[]){
         this.mobiles = mobiles
+        this.totalPrice = this.totalPriceCalculation()
     }
     public setMobiles(newMobiles:Mobile[]):void{
         this.mobiles = newMobiles
@@ -16,5 +17,8 @@ export class MobileCollection{
     }
     public getTotalPrice():number{
         return this.totalPrice
+    }
+    private totalPriceCalculation():number{
+        return this.mobiles.reduce((acum, act) => acum + act.getPrice(), 0)
     }
 }
--- mobileProject/mobileCollectionTest.ts
+++ mobileProject/mobileCollectionTest.ts
@@ -16,3 +16,5 @@ myCollection.setMobiles([new Mobile("Justino", "Huawei", "TS10", "blanco", 135),
 console.log(myCollection.getTotalPrice());
 console.log(myCollection.getMobiles());
 
+let myCollection2 = new MobileCollection([mobile1, mobile2])
+console.log(myCollection2.getTotalPrice());
